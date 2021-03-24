7ca6fb6e020d0979029b256f2e8bd127d6c9ab73
HaythemRomdhani <romdhanihaythem1991@gmail.com>
2021-03-24 11:06:04 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@
 import './App.css';
 import './style.css';
 import myWonderfulImage from "./imageInSrc.jpg"
-import video from "./video.mp4";
+import video from "./Nature.mp4";
 function App() {
   return (
-    <>
-    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
-      <h1 className='title red'>Your name here</h1>
-      <br ></br>
+    <div className="App">
+      <header className="App-header">
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+      <h1 className='title red'>Haythem Romdhani</h1>
+      <br/>
       <img src={myWonderfulImage} alt ='myImage' style={{ height:300}}/>
-      <br ></br>
+      <br/>
       <img src="/imageInPublic.jpg"  alt="myimage" style={{ height:282}}/>
+      <br/>
+    <video src={video}  width="700" height="240" controls="true" />
+    <br/>
     </div>
-    <video controls  src={video} type="video/mp4" style={{width:700, height:240}}></video>
-    </>
+    </header>
+    </div>
   );
 }
 
